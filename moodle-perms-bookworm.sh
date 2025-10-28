bf350cf86f5515800de086b05bc9ca01696ba7bd
Daniele Lolli <daniele.lolli@deltaviadana.it>
2025-10-28 15:02:11 +0100

    data path accordind to TurnKey

--- moodle-perms-bookworm.sh
+++ moodle-perms-bookworm.sh
@@ -18,7 +18,7 @@ set -e  # Exit immediately on error
 
 # Default configurations
 MOODLE_DIR="/var/www/moodle"
-MOODLEDATA_DIR="/var/moodledata"
+MOODLEDATA_DIR="/var/www/moodledata"
 WWW_USER="www-data"
 WWW_GROUP="www-data"
 
